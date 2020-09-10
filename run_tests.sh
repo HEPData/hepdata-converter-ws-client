fcf4770ffad8c3cd90f3d9eeaf7d965ecf665921
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-09-10 10:36:11 +0100

    Increase wait for docker container to start
    1s was not always enough when running locally.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 docker run -d -p 0.0.0.0:8945:5000 --name hepdata-converter-ws-tests hepdata/hepdata-converter-ws
-sleep 1
+sleep 2
 coverage run -m unittest discover hepdata_converter_ws_client/testsuite 'test_*'
 docker stop hepdata-converter-ws-tests
 docker container rm hepdata-converter-ws-tests
